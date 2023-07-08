504b3c8bf2a0854664360745587d61582906c949
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-07-08 10:59:12 +0800

    cosm: user and post view

--- components/user/profile.js
+++ components/user/profile.js
@@ -3,6 +3,8 @@
  * Copyright 2021-Present 唐杰
  * Licensed under the Apache-2.0 license
  */
+import { fresnsConfig, fresnsLang } from '../../api/tool/function';
+
 Component({
   /** 组件的属性列表 **/
   properties: {
@@ -10,5 +12,21 @@ Component({
   },
 
   /** 组件的初始数据 **/
-  data: {},
+  data: {
+    userFollowerName: '',
+    userFollowingName: '',
+  },
+
+  /** 组件生命周期声明对象 **/
+  lifetimes: {
+    attached: async function () {
+      const userFollowerName = await fresnsConfig('user_follower_name');
+      const userFollowingName = await fresnsLang('userFollowing');
+
+      this.setData({
+        userFollowerName: userFollowerName,
+        userFollowingName: userFollowingName,
+      });
+    },
+  },
 });
