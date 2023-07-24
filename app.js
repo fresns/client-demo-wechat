372d43d3f603adcee7bdc53e137c9b9d205d9bc9
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-07-24 14:37:17 +0800

    feat: update manager

--- app.js
+++ app.js
@@ -23,6 +23,39 @@ App({
 
   // 监听小程序初始化
   onLaunch: async function () {
+    // 版本检测
+    if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager();
+
+      updateManager.onCheckForUpdate(function (res) {
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function () {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，重新进入小程序使用新版',
+              success: function (res) {
+                if (res.confirm) {
+                  updateManager.applyUpdate()
+                }
+              }
+            })
+          })
+          updateManager.onUpdateFailed(function () {
+            wx.showModal({
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+            })
+          })
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用本小程序，请升级到最新版微信。'
+      })
+    }
+
+    // 主题配置
     const appBaseInfo = wx.getAppBaseInfo();
     this.onThemeChange(appBaseInfo);
   },
