5a741fa6b97ce7b22b8d4a094ce2147314b3a715
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-09-01 13:37:24 +0800

    build: v2.3.1

--- utils/fresnsGlobalInfo.js
+++ utils/fresnsGlobalInfo.js
@@ -10,7 +10,7 @@ import { cacheGet, cachePut } from './fresnsUtilities';
 
 export class GlobalInfo {
   clientName = 'FresnsWeChat';
-  clientVersion = '2.3.0';
+  clientVersion = '2.3.1';
   theme = null;
 
   // 处理用户主页路径
