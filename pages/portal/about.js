63c5b5d6a2b2fbe00b78bc68f4114f8d516c8975
Jevan Tang <22316055+jevantang@users.noreply.github.com>
2023-09-20 15:09:51 +0800

    chore: privacy

--- pages/portal/about.js
+++ pages/portal/about.js
@@ -13,6 +13,8 @@ Page({
 
   /** 页面的初始数据 **/
   data: {
+    showPrivacy: false,
+
     fresnsVersion: '2.x',
     clientVersion: '1.x',
     appInfo: {},
@@ -36,6 +38,20 @@ Page({
 
   /** 复制公众号 **/
   tapCopyMP: function () {
+    // 判断隐私授权
+    if (wx.canIUse('getPrivacySetting')) {
+      wx.getPrivacySetting({
+        success: (res) => {
+          if (res.needAuthorization) {
+            // 需要弹出隐私协议
+            this.setData({
+              showPrivacy: true,
+            });
+          }
+        },
+      });
+    }
+
     wx.setClipboardData({
       data: 'FresnsCN',
       success: function (res) {
